769fce858ae485f4ffff7eb70094f9b0a3cc3984
BcRikko <s.shin1106@gmail.com>
2019-09-02 09:45:24 +0900

    :green_heart: add test of xyrange

---
+++ test/xyrange.test.ts
@@ -0,0 +1,27 @@
+import { XYRange } from "../src/xyrange";
+import { Range } from "../src/range";
+
+describe('XYRange', () => {
+  const xRange: Range = { min: -100, max: 100 }
+  const yRange: Range = { min: -100, max: 100 }
+  const xyRange = new XYRange({
+    x: xRange,
+    y: yRange
+  })
+
+  test('xRange', () => {
+    expect(xyRange.xRange).toEqual(xRange)
+  })
+
+  test('xRange', () => {
+    expect(xyRange.xRange).toEqual(yRange)
+  })
+
+  test('xDistance', () => {
+    expect(xyRange.xDistance).toBe(200)
+  })
+
+  test('xDistance', () => {
+    expect(xyRange.yDistance).toBe(200)
+  })
+})
